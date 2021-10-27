80149532129249aa402ea19c3df26c4dabd8056a
Bobby Curtis <bobby.curtis@rheodata.com>
2021-10-27 16:29:01 -0400

    removed RHEL test

--- Linux7-RHEL/.vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["vagrant-auto_network",["= 1.0.3"]],["log4r",[">= 0"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]],["vagrant-vmware-desktop",["= 3.0.1"]]],"checksum":"b677cad96b077252feab807bb87cc803f1c1849ce1a2eac454641cf201e34988","vagrant_version":"2.2.18"}
